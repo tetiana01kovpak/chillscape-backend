9cff5bf073cfa8087398fa765d3a900164ee070e
Your Name <alisapodgaynaya023@gmail.com>
2026-03-26 15:46:17 -0600

    add data from user model

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,13 @@
 import User from '../models/user.js';
 
 export const getCurrentUser = async (userId) => {
-  const user = await User.findById(userId).select('_id name email avatar'); //need exact names from user model
+  const user = await User.findById(userId).select(
+    '_id username email password avatar',
+  ); //avatar missing in model
   return user;
 };
 
 export const getUserById = async (userId) => {
-  const user = await User.findById(userId).select('_id name avatar'); //need exact names from user model
+  const user = await User.findById(userId).select('_id username avatar'); //avatar missing in model
   return user;
 };
