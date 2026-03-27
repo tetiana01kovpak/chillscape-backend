f00dc09bbffaae61356da454962a834b1d6dc594
Dmytro <dmytriy.litvin@gmail.com>
2026-03-27 23:08:04 +0200

    done

--- src/controllers/feedbackController.js
+++ src/controllers/feedbackController.js
@@ -1,1 +1,45 @@
-export { };
+import Feedback from "../models/feedback.js";
+
+export const getFeedbacks = async (req, res, next) => {
+  try {
+    const { placeId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
+
+    const skip = (page - 1) * limit;
+
+    const [feedbacks, totalFeedbacks] = await Promise.all([
+      Feedback.find({ place: placeId })
+        .populate("user", "name")
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(Number(limit)),
+
+      Feedback.countDocuments({ place: placeId }),
+    ]);
+
+    const totalPages = Math.ceil(totalFeedbacks / limit);
+
+    res.status(200).json({
+      page: Number(page),
+      perPage: Number(limit),
+      totalFeedbacks,
+      totalPages,
+      feedbacks,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createFeedback = async (req, res, next) => {
+  try {
+    const feedback = await Feedback.create({
+      ...req.body,
+      user: req.user._id,
+    });
+
+    res.status(201).json(feedback);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/validations/feedbackValidation.js
+++ src/validations/feedbackValidation.js
@@ -1,1 +1,19 @@
-export { };
+import { Joi, Segments } from "celebrate";
+
+export const getFeedbacksSchema = {
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number().min(1).default(1),
+    limit: Joi.number().min(1).max(50).default(10),
+  }),
+  [Segments.PARAMS]: Joi.object().keys({
+    placeId: Joi.string().required(),
+  }),
+};
+
+export const createFeedbackSchema = {
+  [Segments.BODY]: Joi.object().keys({
+    place: Joi.string().required(),
+    rating: Joi.number().min(1).max(5).required(),
+    comment: Joi.string().min(3).max(500).required(),
+  }),
+};
