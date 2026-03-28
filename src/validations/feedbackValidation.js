143492c7eac027bbc9a1d8316998122f190be376
Dmytro <dmytriy.litvin@gmail.com>
2026-03-28 21:53:35 +0200

    fixed

--- src/validations/feedbackValidation.js
+++ src/validations/feedbackValidation.js
@@ -6,14 +6,15 @@ export const getFeedbacksSchema = {
     limit: Joi.number().min(1).max(50).default(10),
   }),
   [Segments.PARAMS]: Joi.object().keys({
-    placeId: Joi.string().required(),
+    placeId: Joi.string().hex().length(24).required(),
   }),
 };
 
 export const createFeedbackSchema = {
   [Segments.BODY]: Joi.object().keys({
-    place: Joi.string().required(),
+    place: Joi.string().hex().length(24).required(),
     rating: Joi.number().min(1).max(5).required(),
     comment: Joi.string().min(3).max(500).required(),
   }),
 };
+feedbackSchema.index({ place: 1 });
