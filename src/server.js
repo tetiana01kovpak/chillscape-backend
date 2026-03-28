f1991845ce46806c79100eb864ae627615cb2049
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-03-28 19:49:15 +0200

    setup global error handling and refactor category routes structure

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
-import locationRoutes from './routes/locationRoutes.js';
-
-
-import categoryRoutes from './routes/categoryRoutes.js'; // імпорт роутів категорій
-
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import locationRoutes from './routes/locationRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
 
 dotenv.config();
 
@@ -18,35 +20,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 await connectMongoDB();
+
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/locations', locationRoutes);
-
 
 // routes
-app.use(authRoutes);
-app.use(userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/locations', locationRoutes);
+app.use('/api/categories', categoryRoutes);
 
 // test route
 app.get('/', (req, res) => {
   res.json({ message: 'Backend is running' });
 });
 
-// роут для категорій
-app.use('/api', categoryRoutes);
-
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// base error handler
-app.use((err, req, res, _next) => {
-  const status = err.status || 500;
-  res.status(status).json({ message: err.message || 'Server error' });
-});
+// error handling middlewares
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
