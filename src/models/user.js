6d07fe06d8bc29ea1f4e34d59b03fae65114fe22
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-03-31 00:39:51 +0300

    fix: align schemas with mock data and seed database

--- src/models/user.js
+++ src/models/user.js
@@ -4,43 +4,23 @@ const userSchema = new Schema(
   {
     name: {
       type: String,
-      trim: true,
-    },
-    email: {
-      type: String,
-      unique: true,
       required: true,
       trim: true,
     },
-    password: {
+    avatarUrl: {
       type: String,
       required: true,
-      minlength: 8,
     },
-    avatar: {
-      type: String,
-      required: false,
-      default: 'https://ac.goit.global/fullstack/react/default-avatar.jpg',
+    articlesAmount: {
+      type: Number,
+      default: 0,
     },
-    articlesAmount: { type: Number, default: 0 },
   },
   {
     timestamps: true,
     versionKey: false,
   },
 );
-
-userSchema.methods.toJSON = function () {
-  const obj = this.toObject();
-  delete obj.password;
-  return obj;
-};
-
-userSchema.pre('save', function () {
-  if (!this.name) {
-    this.name = this.email;
-  }
-});
 
 const User = model('User', userSchema);
 
